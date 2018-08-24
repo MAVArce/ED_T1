7fb8465fc8d4588c5c090775f6955ad073f2f2b0
MAVArce <marquinhos_213@hotmail.com>
2018-08-24 15:09:52 -0300

    No comments

--- main.c
+++ main.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct bi{
+typedef struct bi{              //Struct para formar a lista
     int value;
     struct bi* NEXT;
 } DIGIT;
 
-DIGIT* insert(int numb){                    //OK
+DIGIT* insert(int numb){   //Função para inserir um elemento na lista
     DIGIT* pntr;
-    pntr=calloc(1,sizeof(DIGIT));
-    if(pntr == NULL){ printf("Erro na alocacao"); return NULL;}
+    pntr=calloc(1,sizeof(DIGIT));   
+    if(pntr == NULL){ printf("Erro na alocacao"); return NULL;}     //Verificação de erro na alocação
     pntr->value=numb;
     return pntr;
 }
 
-void check(DIGIT** a){                      //OK
+void check(DIGIT** a){                      //Função para retirar os digitos 0 desnecessários na lista
     DIGIT* pntr;
     DIGIT* aux = NULL;
-    for(pntr=*a; pntr!=NULL; pntr=pntr->NEXT){
+    for(pntr=*a; pntr!=NULL; pntr=pntr->NEXT){       //Algoritmo para procurar o último 1 da lista e marcar o endereço
         if(pntr->value==1){
             aux=pntr;
         }
     }
-    if(aux==NULL){
+    if(aux==NULL){                                  //Verificação para caso a lista não tenha nenhum digito 1
         *a=NULL;
         return;
     }
-    if(aux->NEXT!=NULL){
+    if(aux->NEXT!=NULL){                            //Algoritmo para desalocar os 0 no final da lista
         pntr=aux->NEXT;
         aux->NEXT=NULL;
         while(pntr!=NULL && aux!=NULL){    
@@ -50,18 +50,23 @@ int get_size(DIGIT* a){
     return count;
 }
 
-void print(DIGIT* a){                           //OK
+void print(DIGIT** a){                           //OK
     DIGIT* pntr;
+    DIGIT* aux;
 
-    printf("%d ", get_size(a));
+    printf("%d ", get_size(*a));
 
     if(a==NULL){
         printf("0\n");
         return;
     }
-
-    for(pntr=a; pntr!=NULL; pntr=pntr->NEXT)
+    pntr = *a;
+    while(pntr!=NULL){
         printf("%d", pntr->value);
+        aux=pntr;
+        pntr=pntr->NEXT;
+        free(aux);
+    }
     printf("\n");
 } 
 
@@ -97,7 +102,7 @@ void sum(DIGIT** result, DIGIT** a){
             free(aux);
         }
         if(pntr!=NULL){
-            aux = pntr;
+            aux=pntr;
             pntr=pntr->NEXT;
         }
     }
@@ -106,15 +111,9 @@ void sum(DIGIT** result, DIGIT** a){
     }
 }
 
-void total(DIGIT** a, int n){
-    int aux;
-    for(aux = 1; aux<n; aux++){
-        sum(&a[0],&a[aux]);
-    }
-}  
-
 int main(int argc, char const *argv[]){
-    DIGIT** numbers;
+    DIGIT* result;
+    DIGIT* numbers;
     DIGIT* pntr = NULL;
     int numb;
     int n;
@@ -122,38 +121,36 @@ int main(int argc, char const *argv[]){
     int aux;
 
     scanf("%d", &n);
-    numbers=(DIGIT**) calloc(n,sizeof(DIGIT*));
+    result=(DIGIT*)calloc(1,sizeof(DIGIT));
 
     for(aux=0; aux<n; aux++){
         scanf("%d", &size);
         getchar();
        
         numb=getchar()-'0';
-        numbers[aux]=insert(numb);
-        if(numbers[aux]==NULL) return -1;
-        pntr=numbers[aux];
+        numbers=insert(numb);
+        if(numbers==NULL) return -1;
+        pntr=numbers;
 
         for(size--; size>0; size--){
             numb=getchar()-'0';
-            
             if(numb=='\n'-'0')
                 break;
-            
+        
             pntr->NEXT=insert(numb);
-            
-            if(numbers[aux]==NULL) return -1;
+            if(numbers==NULL) 
+                return -1;
             
             pntr=pntr->NEXT;
-            
             if(size==1)
                 while(getchar()!='\n');
         }
-        check(&numbers[aux]);
+        check(&numbers);
+        sum(&result, &numbers);
     }
-    total(numbers, n);
-    check(&numbers[0]);
-    print(numbers[0]);
-    free(numbers);
+    check(&result);
+    print(&result);
+    free(result);
 
     return 0;
 }
